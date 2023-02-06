eb4d2cfed4dc75cb34dd1b65cf107d3e6b41fa4e
sergJess <serg.fantasy@yandex.ru>
2023-02-06 10:20:29 +0300

    feat: add logo

---
+++ src/shared/logo/logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactComponent as Logo } from './logo.svg';
+
+export class Logotype extends React.Component {
+  render(): React.ReactNode {
+    return <Logo />;
+  }
+}
