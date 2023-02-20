b95d8d3c4d58acc5daa41acf2f96b3d622b970fe
sergJess <serg.fantasy@yandex.ru>
2023-02-20 22:14:06 +0300

    feat: add numbers block

--- src/shared/ui/spmc-about/spmc-about.tsx
+++ src/shared/ui/spmc-about/spmc-about.tsx
@@ -4,8 +4,21 @@ import Sausage from './img/saucage.png';
 import { ReactComponent as ShapeLine } from './img/shape-line.svg';
 
 export class SpmcAbout extends React.Component {
-  renderNumbers() {}
+  renderNumberBlock(number: number, text: string): JSX.Element {
+    return (
+      <div className="spmc-number">
+        <div className="spmc-about__number">{`${number}`}</div>
+        <div className="primary-text2">{text}</div>
+      </div>
+    );
+  }
   render() {
+    const texts = [
+      'производственный контроль, основанный на принципах ХАССП',
+      'более 300 видов колбасных и деликатесных изделий, полуфабрикатов и охлажденного мяса',
+      'более 10 тыс. тонн продукции в год',
+      'новейшее высокотехнологичное оборудование',
+    ];
     return (
       <div className="spmc-about">
         <div className="spmc-about__image">
@@ -15,7 +28,11 @@ export class SpmcAbout extends React.Component {
             <span className="spmc-about__image-text_colored">это</span>
           </p>
         </div>
-        <div className="spmc-about__numbers"></div>
+        <div className="spmc-about__numbers">
+          {texts.map((elem, index) => {
+            return this.renderNumberBlock(index + 1, elem);
+          })}
+        </div>
       </div>
     );
   }
