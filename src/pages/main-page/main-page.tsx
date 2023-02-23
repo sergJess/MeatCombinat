b7cd44be4e250e2c52a1bd3d3d0b37ae5c41c517
sergJess <serg.fantasy@yandex.ru>
2023-02-23 20:30:42 +0300

    feat: add block info-list

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -4,6 +4,10 @@ import { SPMCQuality } from 'entities/spmc-quality/spmc-quality';
 import { Slider } from 'features/slider/slider';
 import { SpmcAbout } from 'shared/ui/spmc-about/spmc-about';
 import { Fascinated } from 'shared/ui/fascinated/fascinated';
+import { InfoList } from 'shared/ui/info-list/info-list';
+import IconEcology from './images/info-list/Ico-ecology.svg';
+import IconSocialResponsibility from './images/info-list/Ico-heart.svg';
+import IconMan from './images/info-list/Ico-man.svg';
 
 export class MainPage extends React.Component {
   render(): React.ReactNode {
@@ -15,6 +19,40 @@ export class MainPage extends React.Component {
         </div>
         <SpmcAbout />
         <Fascinated />
+        <div className="main-page__info-list">
+          <InfoList
+            iconSrc={IconEcology}
+            textTitle="Экология"
+            textList={[
+              'Бережное использование природных ресурсов',
+              'Соблюдение экологических законов',
+              'Инициативы в экологии',
+              'Безотходность производства',
+            ]}
+          />
+          <InfoList
+            iconSrc={IconSocialResponsibility}
+            textTitle="Социальная ответственность"
+            textList={[
+              'Прозрачные условия найма и труда',
+              'Охрана здоровья и безопасность',
+              'Контроль качества продукции',
+              'Отсутствие неоплачиваемых переработок',
+              'Безопасность данных и конфиденциальность клиентов',
+              'Премирование сотрудников',
+            ]}
+          />
+          <InfoList
+            iconSrc={IconEcology}
+            textTitle="Управление"
+            textList={[
+              'Структура и гендерный состав руководителей',
+              'Антикоррупционная политика',
+              'Прозрачность налоговой отчетности',
+              'Поддержание комфортной климатической обстановки в компании',
+            ]}
+          />
+        </div>
       </div>
     );
   }
---
+++ src/shared/ui/info-list/info-list.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './info-list.css';
+
+export type TinfoListProps = {
+  iconSrc: string;
+  textTitle: string;
+  textList: Array<string>;
+};
+export class InfoList extends React.Component<TinfoListProps> {
+  constructor(props: TinfoListProps) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="info-list">
+        <div className="info-list__img-inenner">
+          <img src={this.props.iconSrc} alt="" />
+
+          <div className="info-list__img-figures">
+            <div className="info-list__img-circle"></div>
+            <div className="info-list__img-line"></div>
+          </div>
+        </div>
+        <div className="info-list__text-inner">
+          <h3 className="info-list__text-title">{this.props.textTitle}</h3>
+          <ul>
+            {this.props.textList.map((text, index) => {
+              return (
+                <li className="info-list__text-list primary-text2" key={index}>
+                  {text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
